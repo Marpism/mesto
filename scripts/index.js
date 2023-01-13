a587ab5b1c00f2ff08f7ed4e9fb9d933cfa8bbb2
Mary O <marymary-o@ya.ru>
2023-01-13 16:06:54 +0300

    1st review's fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 
 // Открытие/закрытие попапа
 let popup = document.querySelector('.popup');
+let username = document.querySelector('.profile__username');
+let subscription = document.querySelector('.profile__subscription');
+let userNameInput = document.querySelector('#form-input-username');
+let subscriptionInput = document.querySelector('#form-input-subscription');
 
 function popupOpen() {
+  userNameInput.value = username.textContent;
+  subscriptionInput.value = subscription.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -13,31 +19,32 @@ function popupClose() {
 let editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener('click', popupOpen);
 
-let closeButton = document.querySelector('.close-icon');
+let closeButton = document.querySelector('.popup__close-button');
 closeButton.addEventListener('click', popupClose);
 
-// редактирование имени/подписи
-let username = document.querySelector('.profile__username');
-let subscription = document.querySelector('.profile__subscription');
-let submitButton = document.querySelector('.submit-button');
 
-submitButton.addEventListener('click', function() {
-  let userNameInput = document.querySelector('#form-input-username').value;
-  let subscriptionInput = document.querySelector('#form-input-subscription').value;
-  username.textContent = userNameInput;
-  subscription.textContent = subscriptionInput;
+// редактирование имени/подписи
+let form = document.querySelector('form');
+
+function popupSubmit(event) {
+  event.preventDefault();
+  username.textContent = userNameInput.value;
+  subscription.textContent = subscriptionInput.value;
   popupClose()
-});
+}
+
+form.addEventListener('submit', popupSubmit);
+
 
 // лайки
-let likeButtons = document.querySelectorAll('.card__like-icon');
+// let likeButtons = document.querySelectorAll('.card__like-icon');
 
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener('click', function() {
-    if (this.src.slice(-14) === 'heart-icon.svg') {
-      this.src = './images/heart-icon_active.svg';
-    } else { 
-      this.src = './images/heart-icon.svg';
-    }
-  });
-}
+// for (let i = 0; i < likeButtons.length; i++) {
+//   likeButtons[i].addEventListener('click', function() {
+//     if (this.src.slice(-14) === 'heart-icon.svg') {
+//       this.src = './images/heart-icon_active.svg';
+//     } else { 
+//       this.src = './images/heart-icon.svg';
+//     }
+//   });
+// }
